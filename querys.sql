84b634cf775d4145276fdcef45ecc8e292649382
@iago193 <iago.silva6969@gmail.com>
2025-06-05 19:18:34 -0300

    +

--- querys.sql
+++ querys.sql
@@ -8,3 +8,16 @@ create table usuarios(
 	email varchar(150) not null,
 	senha varchar(32) not null
 );
+
+create table tweets(
+	id int not null PRIMARY KEY AUTO_INCREMENT,
+	id_usuario int not null,
+	tweet varchar(140) not null,
+	data datetime default current_timestamp
+);
+
+create table usuarios_seguidores(
+	id int not null primary key auto_increment,
+	id_usuario int not null,
+	id_usuario_seguindo int not null
+);
